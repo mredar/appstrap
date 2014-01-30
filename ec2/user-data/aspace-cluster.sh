69864ae867f647b0abed8ee3616cc62ee4de603b
Mark Redar <mredar@gmail.com>
2014-01-29 16:25:33 -0800

    Run aspace as root, no new user creation.

--- ec2/user-data/aspace-cluster.sh
+++ ec2/user-data/aspace-cluster.sh
@@ -29,31 +29,6 @@ pip install virtualenv
 pip install boto_rsync      # put this in the system python
 pip install awscli  #not sure what version is installed on ec2 image - there is
 
-#######echo "ZONEGET NEXT"
-#######
-#######zone=`wget -q -O -  http://169.254.169.254/latest/meta-data/placement/availability-zone`
-#######oneshorter=${#zone}-1
-#######region=${zone:0:$oneshorter}
-#######echo "ZONE=> $zone, REGION=> $region"
-#######
-######## create a volume and attach. Put /aspace & /aspace.local on this attached volume
-#######command="aws ec2 create-volume
-#######     --region $region
-#######     --availability-zone $zone
-#######     --size 32"
-#######
-#######echo "ebs volume create command $command"
-#######volume=`$command | jq '.VolumeId' -r`
-#######echo $volume
-#######
-#######this_instance=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
-#######
-#######command="aws ec2 attach-volume
-#######     --volume-id $volume
-#######     --instance-id $this_instance
-#######     --device /dev/sdb"
-#######attach=`$command`
-#######
 #mount attached ebs
 mkfs -t ext4 /dev/sdb
 mkdir /aspace
@@ -65,12 +40,12 @@ echo "/dev/sdb  /aspace ext4 defaults 0 2" >> /etc/fstab
 
 su - ec2-user -c 'curl https://raw.github.com/tingletech/appstrap/master/cdl/ucldc-operator-keys.txt >> ~/.ssh/authorized_keys'
 
-useradd aspace
-touch ~aspace/init.sh
-chown aspace:aspace ~aspace/init.sh
-chmod +x ~aspace/init.sh
+##useradd aspace
+##touch ~aspace/init.sh
+##chown aspace:aspace ~aspace/init.sh
+##chmod +x ~aspace/init.sh
 # write the file
-cat > ~aspace/init.sh <<EOSETUP
+cat > /home/ec2-user/init.sh <<EOSETUP
 #!/usr/bin/env bash
 cd
 git clone https://github.com/mredar/appstrap.git
@@ -84,13 +59,12 @@ fi
 ansible-playbook -i host_inventory aspace-cluster-playbook.yml
 EOSETUP
 
-su - aspace -c ~aspace/init.sh
+su - ec2-user -c /home/ec2-user/init.sh
 
-sed 's,/aspace,/home/aspace/aspace,' /home/aspace/aspace/aspace-cluster.init  > /etc/init.d/aspace-cluster
+cp /home/ec2-user/aspace/aspace-cluster.init /etc/init.d/aspace-cluster
 chmod +x /etc/init.d/aspace-cluster
 chkconfig --add aspace-cluster
 
-su - aspace -c ~aspace/init.sh
 #rm ~aspace/init.sh 
 cp ~aspace/init.d-monit /etc/init.d/monit
 chmod 0755 /etc/init.d/monit
