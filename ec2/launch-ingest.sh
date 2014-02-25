e234e15f7733140d80725dea3d655abeae725d86
Mark Redar <mredar@gmail.com>
2014-02-25 15:02:20 -0800

    Start of ingest machine stack.

---
+++ ec2/launch-ingest.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# launch an EC2 server and install application
+
+# this script runs on a machine where "Universal Command Line Interface for Amazon Web Services"
+# https://github.com/aws/aws-cli is installed and is authenticated to amazon web services
+# AWS_ACCESS_KEY and AWS_SECRET_ACCESS_KEY environment variables must be set
+# 
+# This script also requires installation of jq : http://stedolan.github.io/jq/
+
+# like a russia doll, this script 
+#    -   creates a script that runs as root on a brand new amazon linux ec2 
+
+set -eu
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # http://stackoverflow.com/questions/59895
+AMI_EBS="ami-05355a6c"
+EC2_SIZE="t1.micro"
+EC2_SIZE="m1.large"
+ZONE=us-east-1b
+EC2_REGION=us-east-1
+cd $DIR
+
+cat user-data/ingest.sh > ec2_ingest_init.sh
+
+# only on the t1.micro, tune swap
+if [ "$EC2_SIZE" == 't1.micro' ]; then
+  cat >> ec2_ingest_init.sh << DELIM
+# t1.micro's don't come with any swap; let's add 1G
+## to do -- add test for micro
+# http://cloudstory.in/2012/02/adding-swap-space-to-amazon-ec2-linux-micro-instance-to-increase-the-performance/
+# http://www.matb33.me/2012/05/03/wordpress-on-ec2-micro.html
+/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+/sbin/mkswap /var/swap.1
+/sbin/swapon /var/swap.1
+# in case we get rebooted, add swap to fstab
+cat >> /etc/fstab << FSTAB
+/var/swap.1 swap swap defaults 0 0
+FSTAB
+# t1.micro memory optimizations
+DELIM
+
+fi
+
+gzip ec2_ingest_init.sh
+
+command="aws ec2 run-instances 
+     --region $EC2_REGION 
+     --monitoring file://monitoring.json
+     --placement file://placement.json
+     --instance-type $EC2_SIZE                       
+     --count 1:1                                   
+     --image-id $AMI_EBS                             
+     --user-data file://ec2_ingest_init.sh.gz
+     --key-name UCLDC_keypair_0
+     --security-groups Solr
+     --iam-instance-profile Name=s3-readonly"
+
+echo "ec2 launch command $command"
+
+# launch an ec2 and grab the instance id
+instance=`$command | jq '.Instances[0] | .InstanceId' -r`
+
+echo "DONE WITH INSTANCE LAUNCH: $instance"
+
+name_cmd="aws ec2 create-tags --region $EC2_REGION --resources ${instance} --tags Key=Name,Value=UCLDC_ingest Key=project,Value=ucldc"
+tags=`$name_cmd`
+
+echo tags
+echo "DONE WITH NAMING"
+
+# wait for the new ec2 machine to get its hostname
+hostname_cmd="aws ec2 describe-instances --region $EC2_REGION --instance-ids $instance | jq ' .Reservations[0] | .Instances[0] | .PublicDnsName'"
+echo "CMD $hostname_cmd"
+hostname=`aws ec2 describe-instances --region $EC2_REGION --instance-ids $instance | jq ' .Reservations[0] | .Instances[0] | .PublicDnsName'`
+echo "instance started, waiting for hostname"
+while [ "$hostname" = 'null' ]
+  do
+  sleep 15
+  echo "."
+  hostname=`aws ec2 describe-instances --region $EC2_REGION --instance-ids $instance | jq ' .Reservations[0] | .Instances[0] | .PublicDnsName'` 
+  done
+
+echo "INSTANCE:$instance"
+echo $hostname
+
+#TODO: cleanup init file ec2_ingest_init.sh.gz
+
+#Associate with our stanbol-dev elastic ip address
+#retval=`aws ec2 associate-address --region=$EC2_REGION --instance-id $instance --public-ip 54.197.229.87`
+#echo "ASSOCIATE ELASTIC IP ADDRESS RETURNED: $retval"
---
+++ ec2/user-data/ingest.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# cloudinit script
+# https://help.ubuntu.com/community/CloudInit
+# this gets run as root on the amazon machine when it boots up
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+
+# This should be sharable across the various UCLDC machines using 
+# appstrap & pkgsrc to build out the installed software
+# just add your specific packages after this template file
+
+# install packages we need from amazon's repo
+yum -y update			# get the latest security updates
+
+# make sure we use all the attached storage
+resize2fs /dev/xvda1
+
+# install the rest of the software we need
+# git is needed for the build
+yum -y install git 
+
+# handles pkgsrc requirements
+yum -y groupinstall "Development Tools"
+# problem with new setuptools & pip. Need to remove earlier distribute
+# and setuptools manually then install setuptools > 2 (Jan. 03, 2014)
+rm -rf /usr/lib/python2.6/site-packages/distribute*
+rm -rf /usr/lib/python2.6/site-packages/setuptools*
+wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python
+easy_install pip
+pip install virtualenv
+
+pip install awscli  #not sure what version is installed on ec2 image - there is
+#no aws executable
+###yum -y install python-devel  # needed to install(init?) virtualenv with local python
+yum -y install ncurses-devel # needed to install pkgsrc python
+####yum -y install dialog
+###yum -y install openssl-devel
+###yum -y install libjpeg-devel
+###yum -y install freetype-devel
+###yum -y install libtiff-devel
+###yum -y install lcms-devel
+###yum install -y readline-devel libyaml-devel libffi-devel #needed for rvm
+
+su - ec2-user -c 'curl https://raw.github.com/tingletech/appstrap/master/cdl/ucldc-operator-keys.txt >> ~/.ssh/authorized_keys'
+
+useradd ingest
+touch ~ingest/init.sh
+chown ingest:ingest ~ingest/init.sh
+chmod 700 ~ingest/init.sh
+# write the file
+cat > ~ingest/init.sh <<EOSETUP
+#!/usr/bin/env bash
+cd
+git clone -b ingest https://github.com/mredar/appstrap.git
+./appstrap/stacks/stack_ingest #want this to finish, so below works
+EOSETUP
+su - ingest -c ~ingest/init.sh
+rm ~ingest/init.sh 
+cp ~ingest/init.d-monit /etc/init.d/monit
+chmod 0755 /etc/init.d/monit
+chkconfig --add monit
+
