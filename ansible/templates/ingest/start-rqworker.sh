12a04b7fb694355727a490b83fcd2c0ae69c368f
Mark Redar <mredar@gmail.com>
2014-06-19 20:53:34 +0000

    Make these work

--- ansible/templates/ingest/start-rqworker.sh
+++ ansible/templates/ingest/start-rqworker.sh
@@ -1,13 +1,25 @@
 #! /bin/bash
+if [[ -n "$DEBUG" ]]; then 
+  set -x
+fi
+
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+
 cd {{ role_home_dir.stdout }}/code/harvester/
 
+export EMAIL_RETURN_ADDRESS=ucldc@ucop.edu
+
+set +u
 . ./bin/activate
+set -o nounset
 
 if [ -e {{ role_home_dir.stdout }}/log/rqworker-${1}.pid ]; then
     #is running already?
     worker_pid=`/bin/cat {{ role_home_dir.stdout }}/log/rqworker-${1}.pid`
-    ps -A | grep $worker_pid
-    if [ $? -eq 0 ]; then
+    if ps -A | grep -q "$worker_pid"; then
 	echo "worker-${1} appears to be running at PID $worker_pid"
         exit 13
     fi
--- ansible/templates/ingest/stop-rqworker.sh
+++ ansible/templates/ingest/stop-rqworker.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
-set -x
+if [[ -n "$DEBUG" ]]; then 
+  set -x
+fi
+
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+
 /bin/cat {{ role_home_dir.stdout }}/log/rqworker-${1}.pid | /usr/bin/xargs kill
+/bin/rm {{ role_home_dir.stdout }}/log/rqworker-${1}.pid
