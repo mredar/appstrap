f75a6f93b9f827035a9c902e210677251e9a07b2
Mark Redar <mredar@gmail.com>
2014-09-03 12:28:24 -0700

    exit on err, remove ucdavis

--- ansible/templates/aspace/twill/run-aspace-public-load-test.bash
+++ ansible/templates/aspace/twill/run-aspace-public-load-test.bash
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+set -o errexit
 
 twill-fork test-public.tw  --url http://ucm.aspace.cdlib.org --number=30 --processes=3 &
 twill-fork test-public.tw  --url http://ucr.aspace.cdlib.org --number=30 --processes=3 &
@@ -9,4 +10,4 @@ twill-fork test-public.tw  --url http://ucbeda.aspace.cdlib.org --number=30 --pr
 twill-fork test-public.tw  --url http://ucrcmp.aspace.cdlib.org --number=30 --processes=3
 twill-fork test-public.tw  --url http://uclaclark.aspace.cdlib.org --number=30 --processes=3
 twill-fork test-public.tw  --url http://ucmppdc.aspace.cdlib.org --number=30 --processes=3
-twill-fork test-public.tw  --url http://ucdavis.aspace.cdlib.org --number=30 --processes=3
+#SHUTDOWN Aug 2014 twill-fork test-public.tw  --url http://ucdavis.aspace.cdlib.org --number=30 --processes=3
--- ansible/templates/aspace/twill/run-aspace-public-test.bash
+++ ansible/templates/aspace/twill/run-aspace-public-test.bash
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+set -o errexit
 
 twill-sh test-public.tw  --url http://ucm.aspace.cdlib.org
 twill-sh test-public.tw  --url http://ucr.aspace.cdlib.org
@@ -8,5 +9,5 @@ twill-sh test-public.tw  --url http://ucsc.aspace.cdlib.org
 twill-sh test-public.tw  --url http://ucbeda.aspace.cdlib.org
 twill-sh test-public.tw  --url http://ucrcmp.aspace.cdlib.org
 twill-sh test-public.tw  --url http://ucmppdc.aspace.cdlib.org
-twill-sh test-public.tw  --url http://ucdavis.aspace.cdlib.org
 twill-sh test-public.tw  --url http://uclaclark.aspace.cdlib.org
+#SHUTDOWN Aug 2014 twill-sh test-public.tw  --url http://ucdavis.aspace.cdlib.org
